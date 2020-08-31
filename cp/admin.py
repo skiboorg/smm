7787c6f88dce468bf1603ff17271b212892e26dd
xxx <11@11.11>
2020-08-31 12:32:16 +0300

    new pages

--- cp/admin.py
+++ cp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Status)
+admin.site.register(Service)
+admin.site.register(SocialNetwork)
+admin.site.register(Tarif)
+admin.site.register(Order)
